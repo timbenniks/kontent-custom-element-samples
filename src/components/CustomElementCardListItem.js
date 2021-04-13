86d5da0553d53f8fce58b9b6931db14df59b5c59
Rosta Striz <rostislavs@kentico.com>
2021-04-13 19:36:36 +0200

    updated items

--- src/components/CustomElementCardListItem.js
+++ src/components/CustomElementCardListItem.js
@@ -4,13 +4,32 @@ import { trackCustomEvent } from 'gatsby-plugin-google-analytics'
 
 function CustomElementCardListItem({ customElement }) {
   return (
-    <div className="card card--lg-2">
+    < div className="card card--lg-2" >
+      {
+        customElement.core &&
+        <div class="tag core tag--orange">CORE INTEGRATION</div>
+      }
+      <div className="card__content_bottom categories">
+        {customElement.categories &&
+          customElement.categories.map((c, i) => (
+            <span
+              key={i}
+              className="tag"
+              style={{
+                margin: "3px",
+              }}
+            >
+              {c}
+            </span>
+          ))}
+      </div>
       <a
         href={customElement.readmeUrl}
         className="card__link-outer"
         target="_blank"
+        style={{ display: "inline" }}
         rel="noopener noreferrer"
-        onClick={e=>{
+        onClick={e => {
           trackCustomEvent({
             category: "CESG",
             action: "Click Detail",
@@ -20,7 +39,10 @@ function CustomElementCardListItem({ customElement }) {
       ></a>
       <div className="card__content">
         <div className="card__heading  card__heading--small">
-          <h3>{customElement.title}</h3>
+          <h3>
+            {customElement.logoUrl &&
+              <img src={customElement.logoUrl} style={{ width: "20px", display: "inline", verticalAlign: "middle", marginRight: "10px" }}></img>}
+            {customElement.title}</h3>
         </div>
         <div
           className="card__image card__image--no-bg"
@@ -32,24 +54,23 @@ function CustomElementCardListItem({ customElement }) {
           <img src={customElement.thumbnailUrl.publicURL} alt="Screenshot of element in action" />
         </div>
         <div className="card__description">
-          <p>{customElement.description}</p>
-        </div>
-        <div className="card__content-bottom">
-          {customElement.categories &&
-            customElement.categories.map((c, i) => (
-              <span
-                key={i}
-                className="tag tag--orange"
-                style={{
-                  margin: "6px",
-                }}
-              >
-                {c}
-              </span>
-            ))}
+          <p dangerouslySetInnerHTML={{__html: customElement.description}}></p>
         </div>
       </div>
-    </div>
+      <div className="card__content_bottom techs">
+        <a style={{ float: "left", marginLeft: "20px", textDecoration: "none"}} >{customElement.readmeUrl.split("/")[3]}</a>
+        {customElement.tech &&
+          <span
+            className="tag tag--blue"
+            style={{
+              margin: "3px",
+            }}
+          >
+            {customElement.tech}
+          </span>
+        }
+      </div>
+    </div >
   )
 }
 
